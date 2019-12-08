0a44d83aac415afb8f0c7c3d1dbfbae47d701d4d
liuchelx <liuchelx@gmail.com>
2019-12-08 00:07:58 -0800

    second version, but the homepage goes wrong

--- DiscountWeb/HomePage/models.py
+++ DiscountWeb/HomePage/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 
 class User(models.Model):
-    gender = (
-        ('male','female'),
-        )
 
-    Name = models.CharField(max_length=128,unique=True)
+
+    name = models.CharField(max_length=128,unique=True)
     ID = models.IntegerField(default=0)
-    Password = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
-    Gender = models.CharField(max_length=32,choices=gender,default='male')
+    #Gender = models.CharField(max_length=32,choices=gender,default='male')
 
 
     def __str__(self):
         return self.name
+
+class ConfirmString(models.Model):
+    code = models.CharField(max_length=256)
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.name + self.code
+
+    class Meta:
+        ordering = ['-c_time']
+        verbose_name = '确认码'
+        verbose_name_plural = '确认码'
--- DiscountWeb/HomePage/urls.py
+++ DiscountWeb/HomePage/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('comfirm/',views.user_confirm),
+    path('logout/',views.logout),
+    #path('captcha',include('captcha.urls')),
 ]
